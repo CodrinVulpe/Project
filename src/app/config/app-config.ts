1bc57281d06257f2a5971d1a9e40fc05f0d99916
CodrinVulpe <vulpe.codrin@yahoo.com>
2021-09-05 19:54:19 +0300

    Initial commit-frontend+databse

---
+++ src/app/config/app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId:'0oa104w6okoMvWFzx5d7',
+        issuer: 'https://dev-06162590.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile', 'email']
+    }
+}
